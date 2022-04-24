28a5153bfd8fd73c149fe05bcaca4299da2e05d3
bhokaresneha <bhokaresneha20@gmail.com>
2022-04-24 11:14:28 +0530

    Welcome to Line Comparison Computation

---
+++ src/com/bridgelabz/LineComparison.java
@@ -0,0 +1,18 @@
+/* Problem Statement=>
+* Start with Displaying Welcome to Line Comparison Computation Program on Master Branch
+ */
+package com.bridgelabz;
+
+public class LineComparison {
+    static void displayMessage() {
+        System.out.println("Welcome to Line Comparison Computation");
+    }
+
+    public static void main(String[] args) {
+        LineComparison lc=new LineComparison();
+        //Calling static displayMessage method()
+        displayMessage();
+    }
+}
+
+
